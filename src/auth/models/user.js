c0c3bcb4788f68a5f63055141eef3a2fbb82c33f
Md Kamran Taslim <54260472+KamranTaslim@users.noreply.github.com>
2024-07-22 11:10:57 +0530

    Update user.js

--- src/auth/models/user.js
+++ src/auth/models/user.js
@@ -33,6 +33,11 @@ const userSchema = new Schema(
 userSchema.pre("save", async function (next) {
   if (this.isModified("password") || this.isNew) {
     try {
+      // Validate the password before hashing
+      if (!passwordValidator.validator(this.password)) {
+        throw new Error(passwordValidator.message({ value: this.password }));
+      }
+
       const salt = await bcrypt.genSalt(10);
       this.password = await bcrypt.hash(this.password, salt);
       next();
